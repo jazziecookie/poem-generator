70f80b1161cd74b282e6fe742ffbefc761a85944
jazziecookie <sammy680@gmail.com>
2025-06-23 15:24:22 +0100

    Added JS and API

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function getInput(event){
+  event.preventDefault();
+  let userInput = document.querySelector("#user-input");
+callApi(userInput.value);
+}
+
+function callApi(response){
+  let api = `https://api.shecodes.io/ai/v1/generate?prompt=generate_a_short_poem_about_${encodeURIComponent(response)}&context=you_write_in_the_style_of_Charles_Bukowski and output in basic html without doctype&key=ff3f6c762d03ea64t3ab6978450d240o`;
+ axios.get(api).then(outputPoem);
+ console.log(response);
+}
+
+function outputPoem(aiPoem) {
+console.log(aiPoem.data.answer);
+
+new Typewriter("#poem", {
+  strings: `${aiPoem.data.answer}`,
+  autoStart: true,
+  loop: false,
+  delay: 20,
+  cursor: "",
+});
+}
+
+let submitForm = document.querySelector("#submit-form");
+submitForm.addEventListener("submit", getInput);
